3b3cabb7fc32e754699dcc3225e2688c981c6e3d
Jean Luiz <zatzk@outlook.com>
2022-11-30 08:16:58 -0300

    few changes

--- src/components/Cardbar.jsx
+++ src/components/Cardbar.jsx
@@ -1,4 +1,4 @@
-import { SimpleGrid } from "@chakra-ui/react";
+import { SimpleGrid, Container } from "@chakra-ui/react";
 import React from "react";
 import { Card } from "./Card";
 import CardData from "./CardData"
@@ -7,14 +7,24 @@ import CardData from "./CardData"
 export const Cardbar = () => {
   
   return (
-    <SimpleGrid margin={5} mt={40} spacing ={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
-      {CardData.map((data) => (
-            <Card 
-            title={data.title} 
-            icon={data.icon} 
-            />
+    <Container 
+    maxW="container.lg"
+    padding="50px"
+    height="100%"
+    flexWrap="wrap"
+    alignContent="center"
+    mx={{base:0, md:45, lg:85}} mt={40} 
+    >
+
+      <SimpleGrid spacing ={0} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
+        {CardData.map((data) => (
+          <Card 
+          title={data.title} 
+          icon={data.icon} 
+          />
           ))}
-    </SimpleGrid>
+      </SimpleGrid>
+    </Container>
   );
 };
 
